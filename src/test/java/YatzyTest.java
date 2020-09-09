d9f40fc60f2c89e40d6a067328eaa55b1183b8f8
Haroon2206 <56083577+Haroon2206@users.noreply.github.com>
2020-09-09 16:59:07 +0200

    helo

---
+++ src/test/java/YatzyTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YatzyTest {
+    @Test
+    void shouldScoreOnes() {
+        assertEquals(4, new YatzyGame().score(YatzyCategory.ONES, new int[]{1, 1, 2, 1, 1}));
+        assertEquals(2, new YatzyGame().score(YatzyCategory.ONES, new int[]{6, 5, 2, 1, 1}));
+    }
+
+}
