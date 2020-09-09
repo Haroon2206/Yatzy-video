bc135442c42eed0091b20284eb2699a7afb0f60d
furkan2503 <56083285+furkan2503@users.noreply.github.com>
2020-09-09 18:34:05 +0200

    First fail

--- src/main/java/YatzyGame.java
+++ src/main/java/YatzyGame.java
@@ -1,5 +1,5 @@
 public class YatzyGame {
     public int score(YatzyCategory category, int[] dice) {
-        return 2;
+        return 4;
     }
 }
--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -6,6 +6,8 @@ public class YatzyTest {
     @Test
     void shouldScoreOnes() {
         assertEquals(4, new YatzyGame().score(YatzyCategory.ONES, new int[]{1, 1, 2, 1, 1}));
+        assertEquals(2, new YatzyGame().score(YatzyCategory.ONES, new int[]{6, 5, 2, 1, 1}));
+
 
     }
 }
